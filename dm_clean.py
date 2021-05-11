3792685d1d015ff4c4ecc2cdf9903e6083b6981f
0xDADDY <81158699+0xdaddy@users.noreply.github.com>
2021-05-11 21:23:24 +0300

    Update dm_clean.py

--- dm_clean.py
+++ dm_clean.py
@@ -14,7 +14,6 @@ class do:
         while True:
             try:
                 base_url = "https://i.instagram.com/api/v1/direct_v2/inbox/?persistentBadging=true&folder=&limit=20&thread_message_limit=10"
-                #https://i.instagram.com/api/v1/direct_v2/threads/broadcast/text/
                 headers = {}
                 headers['user-agent'] = 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)'
                 res       = requests.get(base_url,headers=headers,cookies=self.cok)
